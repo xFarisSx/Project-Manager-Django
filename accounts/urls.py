6b644bed61af06a5024b6ccfd95876d353c2b803
xFarisSx <faristaleek@gmail.com>
2023-01-16 20:26:09 +0300

    register, profiles

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from accounts.forms import UserLoginForm
+from accounts.views import RegisterView, edit_profile
 from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('login/', LoginView.as_view(authentication_form = UserLoginForm), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', edit_profile, name='profile'),
 ]
